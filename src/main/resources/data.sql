650fe7ecb525985abb08aac2a56f5eb3aaf8d3ea
Neeraj-Kashyap <neeraj1610007@gmail.com>
2026-09-23 03:40:27 +0530

    Added the data.sql containing DB Scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+
+
+CREATE database training_db;
+
+INSERT INTO training_db.training_sessions (session_id, trainee_id, status, version) VALUES
+('1001', '2001', 'COMPLETED',1),
+('1002', '2002', 'IN_PROGRESS',1),
+('1003', '2003', 'IN_PROGRESS',1),
+('1004', '2002', 'COMPLETED',2),
+('1005', '2004', 'COMPLETED',1);
