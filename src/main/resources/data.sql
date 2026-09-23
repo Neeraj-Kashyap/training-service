98eb71f5fb2db552802ea9804350af9ed2d98d6d
Neeraj-Kashyap <neeraj1610007@gmail.com>
2026-09-23 08:49:00 +0530

    Updated the script in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ INSERT INTO training_db.training_sessions (session_id, trainee_id, status, versi
 ('1002', '2002', 'IN_PROGRESS',1),
 ('1003', '2003', 'IN_PROGRESS',1),
 ('1004', '2002', 'COMPLETED',2),
-('1005', '2004', 'COMPLETED',1);
+('1005', '2004', 'IN_PROGRESS',1);
 
 
 SELECT * FROM training_db.training_sessions;
